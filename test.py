571e62c6c898042bbbc24b28b5988e613c2a2ca8
YydWj1314 <yydyyd0318@gmail.com>
2025-11-05 13:01:15 -0800

    Update test.py
    update

--- test.py
+++ test.py
@@ -1,2 +1,25 @@
 
+a = [(5, 3), (2, 1), (5, 2)]
+assert insertion_sort_by_shelf(a) == [(2, 1), (5, 3), (5, 2)]
+
+
+b = [(1, 100), (1, 200), (1, 300)]
+assert insertion_sort_by_shelf(b) == [(1, 100), (1, 200), (1, 300)]
+
+
+c = [(3, 10), (2, 20), (4, 30), (2, 40)]
+assert insertion_sort_by_shelf(c) == [(2, 20), (2, 40), (3, 10), (4, 30)]
+
+
+d = [(1, 7), (2, 8), (3, 9)]
+assert insertion_sort_by_shelf(d) == [(1, 7), (2, 8), (3, 9)]
+
+e = [(3, 9), (2, 8), (1, 7)]
+assert insertion_sort_by_shelf(e) == [(1, 7), (2, 8), (3, 9)]
+
+f = [(1, 7), (2, 8), (3, 9)]
+assert insertion_sort_by_shelf(e) == [(1, 7), (2, 8), (3, 9)]
+
+g = [(1, 7)]
+assert insertion_sort_by_shelf(e) == [(1, 7)]
 
