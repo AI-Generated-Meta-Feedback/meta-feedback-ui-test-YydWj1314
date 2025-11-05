14a8ad5394ef8cb9ea8c2768a3e2c822dec32292
YydWj1314 <yydyyd0318@gmail.com>
2025-11-05 13:05:23 -0800

    Update CODE.py
    update

--- CODE.py
+++ CODE.py
@@ -1,1 +1,13 @@
+a = [(5, 3), (2, 1), (5, 2)]
+n = len(a)
+
+for i in range(1, n):
+    x = a[i]
+    j = i - 1
+    while j >= 0 and (a[j][0] > x[0] or (a[j][0] == x[0] and a[j][1] > x[1])):
+        a[j+1] = a[j]
+        j -= 1
+    a[j+1] = x
+
+print(a)  # [(2, 1), (5, 2), (5, 3)]
 
